7bdfb43f4148befd28a674499097fd311699a10e
Elias Meire <me@eliasmei.re>
2016-06-16 23:48:57 +0200

    stop doing useless builds

--- build.sh
+++ build.sh
@@ -47,8 +47,16 @@ cd ../eliasmeire.github.io.${TARGET_BRANCH}
 git config user.email "eliasmbot@gmail.com"
 git config user.name "Eliasbot"
 
-# add and commit
+# add files
 git add -A .
+
+# If there are no changes to the compiled _site (e.g. this is a README update) then just bail.
+if [ -z `git diff --cached --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# commit files
 git commit -am "Build from ${SOURCE_BRANCH} branch | Deployed by TravisCI (Build #$TRAVIS_BUILD_NUMBER)"
 
 # force push to github
